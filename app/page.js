29dcd426ff36c1338f2656d1cf5b30ea29b93978
Anuj Patel <anujpatel1326@gmail.com>
2025-04-27 19:18:25 -0400

    feat: added Clerk Toolbar

--- app/page.js
+++ app/page.js
@@ -1,9 +1,27 @@
 import Image from "next/image";
-
+import getStripe from "@/utils/get-stripe";
+import {SignedIn, SignedOut, UserButton} from "@clerk/nextjs";
+import {Container, AppBar, Toolbar, Typography, Button} from "@mui/material";
+import Head from "next/head";
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Container maxWidth="lg">
+      <Head>
+        <title>QuizWhiz</title>
+        <meta name="description" content="QuizWhiz! A Flashcard SaaS App" />
+      </Head>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">QuizWhiz</Typography>
+          <SignedOut>
+            <Button color="inherit">Login</Button>
+            <Button color="inherit">Register</Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </Toolbar>
+      </AppBar>
+    </Container>
   )
 }
